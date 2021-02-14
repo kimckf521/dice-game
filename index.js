bf0d489994bac387e00604c4b35b39f4561dcd31
Kim Entropy <dickson2005ckf@Kims-MacBook-Pro.local>
2021-02-15 02:48:51 +1100

    Initial Version

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(6 * Math.random()) + 1; //1-6;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(6 * Math.random()) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🍣Player 1 Wins!";
+} else if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "🍻Draw, Super!";
+} else {
+  document.querySelector("h1").innerHTML = "🍡Player 2 Wins!";
+}
